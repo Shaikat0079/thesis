4dce8729a2b44ae925f73530dda515d4de3238d7
Shaikat0079 <baruashaikat00790@gmail.com>
2022-08-07 21:27:22 +0600

    Added filters on the poisson noise images

--- noise.py
+++ noise.py
@@ -21,3 +21,23 @@ with Image(filename="desktop.jpg") as img:
     # Here it generated laplacian noise
     img.noise("laplacian", attenuate=1.0)
     img.save(filename="noisedesktop2.jpg")
+
+
+import cv2
+
+img = cv2.imread('noiseDesktop.jpg')
+
+# Apply bilateral filter with d = 15,
+sigmaColor = sigmaSpace = 75.
+bilateral = cv2.bilateralFilter(img, 15, 75, 75)
+# Save the output.
+cv2.imwrite('desktop_bilateral.jpg', bilateral)
+# Apply Average filter
+average = cv2.blur(img, (5, 5))
+# Save the output.
+cv2.imwrite('desktop_average.jpg', average)
+# Apply Gaussian Filter
+gaussian = cv2.GaussianBlur(img, (5, 5), 0)
+cv2.imwrite('desktop_gaussian.jpg', gaussian)
+median_filter = cv2.medianBlur(img, 5)
+cv2.imwrite('desktop_median.jpg', median_filter)
