140cecd585c9b4e2b04a6c92821c83c6b642ebfd
Shaikat0079 <baruashaikat00790@gmail.com>
2022-08-07 21:09:16 +0600

    Noise added to the images

---
+++ noise.py
@@ -0,0 +1,23 @@
+from statistics import median
+from numpy import average
+
+
+# Import Image from wand.image module
+from wand.image import Image
+
+# Read image using Image() function
+with Image(filename="desktop.jpg") as img:
+
+    # Generate noise image using spread() function
+    # Here it generated poisson noise
+    img.noise("poisson", attenuate=0.9)
+    img.save(filename="noisedesktop.jpg")
+
+
+# Read image using Image() function
+with Image(filename="desktop.jpg") as img:
+
+    # Generate noise image using spread() function
+    # Here it generated laplacian noise
+    img.noise("laplacian", attenuate=1.0)
+    img.save(filename="noisedesktop2.jpg")
